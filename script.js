dab01cc258a0f1fb4d87dbfd979893da805ac153
Jakub Osuchowski <70976262+chunkERR@users.noreply.github.com>
2022-11-09 16:15:09 +0100

    form now works and adds divs

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
 const books = document.querySelector(".books");
+const formTitle = document.querySelector(".title")
+const formAuthor = document.querySelector(".author")
+const formPages = document.querySelector(".pages")
+const formRead = document.querySelector("#read")
+const submit = document.querySelector(".submit")
+const form = document.querySelector(".new-book")
+
 
 // array of books
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+// function addBookToLibrary(title, author, pages, read) {
+//   let book = new Book(title, author, pages, read);
+//   myLibrary.push(book);
+// }
+
+
+function addBookToLibrary(event) {
+  event.preventDefault();
+  let book = {
+title: formTitle.value,
+author: formAuthor.value,
+pages: formPages.value,
+read: formRead.value
+  }
+
+myLibrary.push(book);
+document.querySelector(".new-book").reset();
+console.log(myLibrary);
+displayBooks();
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  let book = new Book(title, author, pages, read);
-  myLibrary.push(book);
-}
 
 function displayBooks() {
+  const removeCards = document.querySelectorAll('.card');
+  for (let i = 0; i < removeCards.length; i++) {
+    removeCards[i].remove();
+  }
   myLibrary.forEach((element) => {
     const card = document.createElement("div");
     card.classList.add("card");
     books.appendChild(card);
+    for (let key in element) {
+      console.log(`${key} : ${element[key]}`)
+      const cardContent = document.createElement("p")
+      cardContent.innerHTML = (`${key} : ${element[key]}`)
+      card.appendChild(cardContent);
+
+    }
   });
 }
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
 
-displayBooks(myLibrary);
+submit.addEventListener("click", addBookToLibrary)
+
+
+// addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
+// addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
+// addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
+// addBookToLibrary("The Hobbit", "J.R.R Tolkien", 300, true);
+// addBookToLibrary("Lord of the Rings", "J.R.R Tolkien", 1000, false);
+
+
+// displayBooks(myLibrary);
